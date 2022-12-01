7cd4557aeaa3d8033fb259f278ebe4ecebfc6364
Hasnain Cheena <hasnaincheena.hc@gmail.com>
2022-12-01 21:08:17 +1100

    day 1 complete

--- Day 1/day_1.py
+++ Day 1/day_1.py
@@ -1,4 +1,6 @@
 filename = r"D:\Projects\advent_of_code_2022\Day 1\day_1_part_1_puzzle_input.txt"
+
+## PART 1
 with open(filename) as file:
     puzzle_input = file.readlines()
     elves_calories = [] #index represent elf number
@@ -14,3 +16,7 @@ with open(filename) as file:
             calorie_count = 0
 
     print("Highest calorie count: ", max(elves_calories))
+
+## PART 2
+sorted_elves_calories = sorted(elves_calories, reverse=True)
+print("Top 3 elves have a total count of ", sum(sorted_elves_calories[:3]))
