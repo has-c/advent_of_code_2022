b566ec8d2c24eba5a6a71081a67b25f35aa63968
Hasnain Cheena <hasnaincheena.hc@gmail.com>
2022-12-05 22:09:46 +1100

    day 4 complete

--- Day 4/day_4.py
+++ Day 4/day_4.py
@@ -15,6 +15,18 @@ def check_overlapping(
     else:
         return "Not Overlapping"
 
+def check_no_overlap(
+    assignment_1_min,
+    assignment_1_max,
+    assignment_2_min,
+    assignment_2_max
+):
+
+    if assignment_1_max < assignment_2_min or assignment_2_max < assignment_1_min:
+        return "No overlap"
+    else:
+        return "Any overlap"
+
 filename = r"D:\Projects\advent_of_code_2022\Day 4\day_4_puzzle.txt"
 raw_assignment_data = pd.read_csv(filename, sep=",", names=['assignment_1', 'assignment_2'])
 assignment_1_min_max_values = raw_assignment_data["assignment_1"].str.split("-", expand=True).copy().astype(int)
@@ -36,4 +48,15 @@ assignment_values["overlapping"] = assignment_values.apply(lambda x: check_overl
     x["max_value_2"]),axis=1)
 
 num_pairs_overlapping = assignment_values[assignment_values["overlapping"] != "Not Overlapping"].shape[0]
+print("Pairs overlapping ", num_pairs_overlapping)
+
+## PART 2
+# Find all pairs that are overlapping
+assignment_values["any_overlap"] = assignment_values.apply(lambda x: check_no_overlap(
+    x["min_value_1"],
+    x["max_value_1"],
+    x["min_value_2"],
+    x["max_value_2"]),axis=1)
+
+num_overlap_pairs = assignment_values[assignment_values["any_overlap"] != "No overlap"].shape[0]
 print("Pairs overlapping ", num_pairs_overlapping)
